f3911d73faf595a26c67efaaf2aa457828ba5301
Yifang Ma <yifang@nearprotocol.com>
2021-01-25 16:43:58 -0800

    update staking rewards

--- src/utils/term.js
+++ src/utils/term.js
@@ -8,6 +8,7 @@ export const term = {
   next_validators: "The number of validating node in next epoch.",
   network_gdp: "GDP for network. It is calculated by current total supply minus the initial supply.",
   inflation_rate: "Real time inflation rate for NEAR token.",
-  gas_fee: "Total gas fee for transactions. Its starting date is 2020-07-21"
+  gas_fee: "Total gas fee for transactions. Its starting date is 2020-07-21",
+  total_staking_rewards: "Total amount of staking rewards from rewards starts date."
 }
 
