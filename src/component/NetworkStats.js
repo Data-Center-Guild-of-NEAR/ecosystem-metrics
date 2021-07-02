5a4efaf3b24cc9d38c6f31572df3fc164ba33c39
Yifang Ma <yifang@nearprotocol.com>
2021-07-02 15:09:37 +0800

    add more metrics

--- src/component/NetworkStats.js
+++ src/component/NetworkStats.js
@@ -1,95 +1,182 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React, { useState, useEffect, useCallback } from "react";
-import BN from "bn.js";
+import React, { useState, useEffect, useCallback } from 'react';
+import BN from 'bn.js';
 
-import StatsApi from "../explorer-api/stats";
-import { nearRpc } from "../api-js/connect";
-import { NEAR_NOMINATION } from "near-api-js/lib/utils/format";
+import StatsApi from '../explorer-api/stats';
+import { nearRpc } from '../api-js/connect';
+import {
+  NEAR_NOMINATION,
+  formatNearAmount,
+} from 'near-api-js/lib/utils/format';
 
-import { TERA_GAS_UNIT, INITIAL_SUPPLY } from "../utils/const";
-import Tooltip from "../utils/Tooltip";
-import { term } from "../utils/term";
-import { formatWithCommas } from "../utils/convert";
+import { TERA_GAS_UNIT, INITIAL_SUPPLY } from '../utils/const';
+import Tooltip from '../utils/Tooltip';
+import { term } from '../utils/term';
+import { formatWithCommas } from '../utils/convert';
 
 export default () => {
   const [gdp, setGDP] = useState('');
-  const [gdpWeekAgo, setGDPWeekAgo] = useState('')
+  const [gdpWeekAgo, setGDPWeekAgo] = useState('');
 
   const [gasFee, setFee] = useState('');
   const [gasFeeWeekAgo, setFeeWeekAgo] = useState('');
 
   const [stakeReward, setStaking] = useState('');
-  const [stakeRewardWeekAgo, setStakeWeekAgo] = useState('')
+  const [stakeRewardWeekAgo, setStakeWeekAgo] = useState('');
 
-  const [totalDeposit, setDeposit] = useState('')
-  const [totalDepositWeekAgo, setDepositWeekAgo] = useState('')
+  const [totalDeposit, setDeposit] = useState('');
+  const [totalDepositWeekAgo, setDepositWeekAgo] = useState('');
+
+  const [circulatedSupply, setCS] = useState('');
+  const [csWeekAgo, setCSWeekAgo] = useState('');
+
+  const [stake, setStake] = useState('');
+  const [intersection, setIntersection] = useState('');
 
   const Subscription = useCallback(async () => {
-    let { totalGas, weekAgoGas } = await new StatsApi().teraGasAggregatedByDate()
+    let { totalGas, weekAgoGas } =
+      await new StatsApi().teraGasAggregatedByDate();
     // total inflation
-    let block = await nearRpc.sendJsonRpc("block",{"finality": "final"})
-    let gasPrice = block.header.gas_price
-    let totalSupply = block.header.total_supply
-    let gdp = new BN(totalSupply).sub(new BN(INITIAL_SUPPLY)).div(new BN(NEAR_NOMINATION))
-    let height = block.header.height - 604800
-    let prevTotalSupply = await new StatsApi().getTotalSupply(height)
-    let preGdp = new BN(prevTotalSupply).sub(new BN(INITIAL_SUPPLY)).div(new BN(NEAR_NOMINATION))
+    let block = await nearRpc.sendJsonRpc('block', { finality: 'final' });
+    let gasPrice = block.header.gas_price;
+    let totalSupply = block.header.total_supply;
+    let gdp = new BN(totalSupply)
+      .sub(new BN(INITIAL_SUPPLY))
+      .div(new BN(NEAR_NOMINATION));
+    let height = block.header.height - 604800;
+    let prevTotalSupply = await new StatsApi().getTotalSupply(height);
+    let preGdp = new BN(prevTotalSupply)
+      .sub(new BN(INITIAL_SUPPLY))
+      .div(new BN(NEAR_NOMINATION));
     //fee
-    let fee = new BN(totalGas).mul(new BN(TERA_GAS_UNIT)).mul(new BN(gasPrice)).div(new BN(NEAR_NOMINATION))
-    let weekAgoFee = new BN(weekAgoGas).mul(new BN(TERA_GAS_UNIT)).mul(new BN(gasPrice)).div(new BN(NEAR_NOMINATION))
+    let fee = new BN(totalGas)
+      .mul(new BN(TERA_GAS_UNIT))
+      .mul(new BN(gasPrice))
+      .div(new BN(NEAR_NOMINATION));
+    let weekAgoFee = new BN(weekAgoGas)
+      .mul(new BN(TERA_GAS_UNIT))
+      .mul(new BN(gasPrice))
+      .div(new BN(NEAR_NOMINATION));
 
     //total deposit
-    let depositList = await new StatsApi().depositAmountAggregatedByDate()
-    let totalDeposit = depositList.map(
-      (account) => new BN(account.depositAmount).div(new BN(NEAR_NOMINATION))
-    ).reduce((deposit, current) => current.add(deposit), new BN(0))
-    let weekAgoDeposit = depositList.slice(0, depositList.length - 7).map(
-      (account) => new BN(account.depositAmount).div(new BN(NEAR_NOMINATION))
-    ).reduce((deposit, current) => current.add(deposit), new BN(0))
+    let depositList = await new StatsApi().depositAmountAggregatedByDate();
+    let totalDeposit = depositList
+      .map((account) =>
+        new BN(account.depositAmount).div(new BN(NEAR_NOMINATION))
+      )
+      .reduce((deposit, current) => current.add(deposit), new BN(0));
+    let weekAgoDeposit = depositList
+      .slice(0, depositList.length - 7)
+      .map((account) =>
+        new BN(account.depositAmount).div(new BN(NEAR_NOMINATION))
+      )
+      .reduce((deposit, current) => current.add(deposit), new BN(0));
 
+    // stake
+    let res = await nearRpc.sendJsonRpc('validators', [null]);
+    let validators = res.current_validators;
+    let stake = validators
+      .map((va) => va.stake)
+      .reduce((prev, curr) => new BN(prev).add(new BN(curr)), new BN('0'));
+    console.log(stake.toString());
+    // settings
+    setGDP(gdp);
+    setGDPWeekAgo(preGdp);
 
-    setGDP(gdp)
-    setGDPWeekAgo(preGdp)
+    setFee(fee);
+    setFeeWeekAgo(weekAgoFee);
 
-    setFee(fee)
-    setFeeWeekAgo(weekAgoFee)
+    setStaking(gdp.muln(0.95));
+    setStakeWeekAgo(preGdp.muln(0.95));
 
-    setStaking(gdp.add(fee))
-    setStakeWeekAgo(preGdp.add(weekAgoFee))
+    setDeposit(totalDeposit);
+    setDepositWeekAgo(weekAgoDeposit);
 
-    setDeposit(totalDeposit)
-    setDepositWeekAgo(weekAgoDeposit)
+    setCS(new BN(totalSupply).div(new BN(NEAR_NOMINATION)));
+    setCSWeekAgo(new BN(prevTotalSupply).div(new BN(NEAR_NOMINATION)));
+
+    setStake(stake);
+    setIntersection(
+      new BN(totalSupply).sub(stake).div(new BN(NEAR_NOMINATION))
+    );
   }, []);
 
   useEffect(() => Subscription(), [Subscription]);
 
-  return <div style={{textAlign: "left"}}>
-          <h4>Network Situation</h4>
-          <div>Total Inflation <Tooltip text={term.network_gdp} /> : <strong className="green">{formatWithCommas(gdp.toString())} Ⓝ</strong>
-              {gdpWeekAgo && <Diff current={gdp} prev={gdpWeekAgo} />}</div>
-          <div>Total staking rewards <Tooltip text={term.total_staking_rewards} /> : <strong className="green">{formatWithCommas(stakeReward.toString())} Ⓝ</strong>
-              {stakeRewardWeekAgo && <Diff current={stakeReward} prev={stakeRewardWeekAgo} />}</div>
-          <div>Inception to date of Gas Fee <Tooltip text={term.gas_fee} /> : <strong className="green">{formatWithCommas(gasFee)} Ⓝ </strong>
-              {gasFeeWeekAgo && <Diff current={gasFee} prev={gasFeeWeekAgo} />}</div>
-          <div>Total token volume transacted:  <Tooltip text={term.total_deposit} /> : <strong className="green">{formatWithCommas(totalDeposit.toString())} Ⓝ</strong>
-              {totalDepositWeekAgo && <Diff current={totalDeposit} prev={totalDepositWeekAgo} />}</div>
-        </div>
-}
-
-const Diff = ({current, prev}) => {
-  let diff = ((current.sub(prev).muln(1000000).div(prev).toNumber())/10000).toFixed(2)
-  let signal = current.gt(prev) ? '+' : '-'
   return (
-    <span 
+    <div style={{ textAlign: 'left' }}>
+      <h4>Network Situation</h4>
+      <div>
+        <strong>Total circulating supply </strong>{' '}
+        <Tooltip text={term.circulating_supply} /> :{' '}
+        <strong className="green">
+          {formatWithCommas(circulatedSupply.toString())} Ⓝ
+        </strong>
+        {csWeekAgo && <Diff current={circulatedSupply} prev={csWeekAgo} />}
+      </div>
+      <div>
+        <strong>Total stake </strong> <Tooltip text={term.total_stake} /> :{' '}
+        <strong className="green">
+          {formatNearAmount(stake.toString(), 0)} Ⓝ
+        </strong>
+      </div>
+      <div>
+        <strong>
+          Intersections between lockups/circulating supply and staking{' '}
+        </strong>{' '}
+        <Tooltip text={term.intersection} /> :{' '}
+        <strong className="green">
+          {formatWithCommas(intersection.toString())} Ⓝ
+        </strong>
+      </div>
+      <div>
+        Total Inflation <Tooltip text={term.network_gdp} /> :{' '}
+        <strong className="green">{formatWithCommas(gdp.toString())} Ⓝ</strong>
+        {gdpWeekAgo && <Diff current={gdp} prev={gdpWeekAgo} />}
+      </div>
+      <div>
+        Total staking rewards <Tooltip text={term.total_staking_rewards} /> :{' '}
+        <strong className="green">
+          {formatWithCommas(stakeReward.toString())} Ⓝ
+        </strong>
+        {stakeRewardWeekAgo && (
+          <Diff current={stakeReward} prev={stakeRewardWeekAgo} />
+        )}
+      </div>
+      <div>
+        Inception to date of Gas Fee <Tooltip text={term.gas_fee} /> :{' '}
+        <strong className="green">{formatWithCommas(gasFee)} Ⓝ </strong>
+        {gasFeeWeekAgo && <Diff current={gasFee} prev={gasFeeWeekAgo} />}
+      </div>
+      <div>
+        Total token volume transacted: <Tooltip text={term.total_deposit} /> :{' '}
+        <strong className="green">
+          {formatWithCommas(totalDeposit.toString())} Ⓝ
+        </strong>
+        {totalDepositWeekAgo && (
+          <Diff current={totalDeposit} prev={totalDepositWeekAgo} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+const Diff = ({ current, prev }) => {
+  let diff = (
+    current.sub(prev).muln(1000000).div(prev).toNumber() / 10000
+  ).toFixed(2);
+  let signal = current.gt(prev) ? '+' : '-';
+  return (
+    <span
       style={{
-        background: "#C2FCE0",
-        borderRadius: "4px",
-        color: current.gt(prev) ? "#008D6A" : "#f5c7c1",
-        marginLeft: "5px",
-        paddingLeft: "3px"
+        background: '#C2FCE0',
+        borderRadius: '4px',
+        color: current.gt(prev) ? '#008D6A' : '#f5c7c1',
+        marginLeft: '5px',
+        paddingLeft: '3px',
       }}
     >
       {signal} {diff}%
     </span>
-  )
-}
+  );
+};
--- src/utils/term.js
+++ src/utils/term.js
@@ -1,21 +1,34 @@
 export const term = {
-  monthly_active_developer: "The count for CLI non-validator users with at least 1 activity in a month.",
-  weekly_active_developer: "The count for CLI non-validator users with at least 1 activity in a week.",
-  current_active_accounts: "The count for active accounts in 24 hours. Active account is defined as account that have executive receipts.",
-  weekly_active_accounts: "The count for active accounts aggregated by weekly.",
-  current_validators: "The number of validating node in current epoch.",
-  current_proposals: "The number of nodes that will be validated in next epoch.",
-  next_validators: "The number of validating node in next epoch.",
-  network_gdp: "Inflation for network. It is calculated by current total supply minus the initial supply.",
-  inflation_rate: "Real time inflation rate for NEAR token.",
-  gas_fee: "Total gas fee for transactions. Its starting date is 2020-07-21.",
-  total_staking_rewards: "Total amount of staking rewards from rewards starts date.",
-  total_deposit: "Total token transaction amount from transfer and function call",
-  deposit_by_day: "Token transaction amount from transfer and function call aggregated by day",
-  monthly_github_developer: "Number of developer contribute to NEAR github repo within a month",
-  weekly_github_developer: "Number of developer contribute to NEAR github repo within a week",
-  partner_count: "Company raising in NEAR ecosystem",
-  transaction_count: "transaction count aggregated by day",
-  new_account_count:  "The count for new accounts in 24 hours. ",
-}
-
+  monthly_active_developer:
+    'The count for CLI non-validator users with at least 1 activity in a month.',
+  weekly_active_developer:
+    'The count for CLI non-validator users with at least 1 activity in a week.',
+  current_active_accounts:
+    'The count for active accounts in 24 hours. Active account is defined as account that have executive receipts.',
+  weekly_active_accounts: 'The count for active accounts aggregated by weekly.',
+  current_validators: 'The number of validating node in current epoch.',
+  current_proposals:
+    'The number of nodes that will be validated in next epoch.',
+  next_validators: 'The number of validating node in next epoch.',
+  network_gdp:
+    'Inflation for network. It is calculated by current total supply minus the initial supply.',
+  circulating_supply:
+    'Total circulating supply after genesis supply with staking rewords.',
+  total_stake: ' Total staked token amount in current epoch.',
+  intersection: 'Total circulating supply - total stake',
+  inflation_rate: 'Real time inflation rate for NEAR token.',
+  gas_fee: 'Total gas fee for transactions. Its starting date is 2020-07-21.',
+  total_staking_rewards:
+    'According to NEAR economy, validator rewards is 95% of total created new coins.',
+  total_deposit:
+    'Total token transaction amount from transfer and function call.',
+  deposit_by_day:
+    'Token transaction amount from transfer and function call aggregated by day.',
+  monthly_github_developer:
+    'Number of developer contribute to NEAR github repo within a month.',
+  weekly_github_developer:
+    'Number of developer contribute to NEAR github repo within a week.',
+  partner_count: 'Company raising in NEAR ecosystem.',
+  transaction_count: 'transaction count aggregated by day.',
+  new_account_count: 'The count for new accounts in 24 hours. ',
+};
