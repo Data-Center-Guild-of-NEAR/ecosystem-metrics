836f8cb274ad61bc478acb7419e97833bb1213f3
Yifang Ma <yifang@nearprotocol.com>
2021-10-08 10:44:40 +0800

    update first dapp

--- src/component/FirstDappDailyTotal.js
+++ src/component/FirstDappDailyTotal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactEcharts from 'echarts-for-react';
 
-import firstDappDailyTotal from '../history/first_dapp_count_daily_total_9_30.json';
+import firstDappDailyTotal from '../history/first_dapp_count_daily_total_10_06.json';
 
 const DailyTotalCountFristDapp = () => {
   const chartStyle = {
@@ -78,7 +78,7 @@ const DailyTotalCountFristDapp = () => {
   return (
     <>
       <h2>Daily Total First Time Dapp granted By User</h2>
-      <p>collected until 2021-09-30</p>
+      <p>collected until 2021-10-06</p>
       <ReactEcharts option={getOption(date, count)} style={chartStyle} />
     </>
   );
